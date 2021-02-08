093e77f4856cc665b6cda653ce860d2ac8c04992
ckdtjs505 <ckdtjs505@naver.com>
2021-02-08 22:22:39 +0900

    feat : create loader and presenter

--- src/router/movie/moviePresenter.js
+++ src/router/movie/moviePresenter.js
@@ -1,6 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Seaction from "../../components/Seaction";
+import Loader from "../../components/loader";
+
+const Container = styled.div`
+  padding: 0px 25px;
+`;
 
 const MoviePresenter = ({
   loading,
@@ -10,7 +16,36 @@ const MoviePresenter = ({
   movieUpComing,
   movieDetail,
   error
-}) => <div>Movie</div>;
+}) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {moviePopular && moviePopular.length > 0 && (
+        <Seaction title="Popular">
+          {moviePopular.map(movie => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Seaction>
+      )}
+
+      {movieTopRated && movieTopRated.length > 0 && (
+        <Seaction title="TopRated">
+          {movieTopRated.map(movie => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Seaction>
+      )}
+
+      {movieNowPlaying && movieNowPlaying.length > 0 && (
+        <Seaction title="NowPlaying">
+          {movieNowPlaying.map(movie => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Seaction>
+      )}
+    </Container>
+  );
 
 MoviePresenter.propTypes = {
   movieNowPlaying: PropTypes.array,
--- src/router/tv/tvPresenter.js
+++ src/router/tv/tvPresenter.js
@@ -1,8 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Seaction from "../../components/Seaction";
+import Loader from "../../components/loader";
 
-const TVPresenter = ({ loading, tvTopRated, tvPopular, tvAringToday, error }) => <div>tv</div>;
+const Container = styled.div`
+  padding: 0px 30px;
+`;
+
+const TVPresenter = ({ loading, tvTopRated, tvPopular, tvAringToday, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {tvTopRated && tvTopRated.length > 0 && (
+        <Seaction title="TV Top Rated">
+          {tvTopRated.map(tv => (
+            <span key={tv.id}>{tv.name}</span>
+          ))}
+        </Seaction>
+      )}
+
+      {tvPopular && tvPopular.length > 0 && (
+        <Seaction title="TV Popular">
+          {tvPopular.map(tv => (
+            <span key={tv.id}>{tv.name}</span>
+          ))}
+        </Seaction>
+      )}
+
+      {tvAringToday && tvAringToday.length > 0 && (
+        <Seaction title="TV Aring Today">
+          {tvAringToday.map(tv => (
+            <span key={tv.id}>{tv.name}</span>
+          ))}
+        </Seaction>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
